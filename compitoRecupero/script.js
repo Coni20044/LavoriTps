bddef23cf3f45801a68015cf6d0d97993bc8510c
MR POLLICIONE <63495781+Coni20044@users.noreply.github.com>
2026-01-09 11:46:22 +0100

    Fix XMLHttpRequest handling and improve readability

--- compitoRecupero/script.js
+++ compitoRecupero/script.js
@@ -1,18 +1,21 @@
 //creo un oggetto XMLHttpRequest
+
 var richiesta = new XMLHttpRequest(); //<--- serve per leggere un file esterno (in questo caso il Json prodotti)
 //qua definisco cosa fare quando la risposta arriva
-richiesta.onreadystatechange = function(){
+richiesta.onreadystatechange = function() {
+
     //readyState 4 = operazione completata
     //status 200 = tutto OK
-    if(richiesta.readyState === 4 && richiesta.status === 200){
+    if(richiesta.readyState == 4 && richiesta.status == 200){
         //converto il testo JSON in oggetti JS utilizzabili
         var prodotti = JSON.parse(richiesta.responseText);
         //prendiamo il div(contenitore nell' index) dove stampare i dati
         var contenuto = document.getElementById("contenuto");
 
         //Ciclo tutti i prodotti scorrendoli 1 alla volta
-        prodotti.forEach(function (prodotto) {
-            
+        prodotti.forEach(function (prodotto) { 
+
+ 
 
             contenuto.innerHTML +=
                 "<div>" +
@@ -24,7 +27,7 @@ richiesta.onreadystatechange = function(){
                 "</div>";
             // IinnerHTML scrive html dinamico nella pagina
     
-        });
+        }); 
     }
 };
 // Apro la richiesta verso il file JSON
